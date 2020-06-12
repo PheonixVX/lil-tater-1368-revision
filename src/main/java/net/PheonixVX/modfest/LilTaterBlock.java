42c8d666ac31f5d4215480f98bddfc44e411f154
PheonixVX <TheGrimReaper.VX@gmail.com>
2020-06-12 18:23:02 -0400

    Finished making lil tater tot block

---
+++ src/main/java/net/PheonixVX/modfest/LilTaterBlock.java
@@ -0,0 +1,36 @@
+package net.PheonixVX.modfest;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
+
+public class LilTaterBlock extends Block {
+
+    public LilTaterBlock(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public void appendProperties(StateManager.Builder builder) {
+        builder.add(Properties.FACING);
+    }
+
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return (BlockState)((BlockState)this.getDefaultState().with(Properties.FACING, ctx.getPlayerLookDirection().getOpposite()));
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext stx) {
+        //Block.createCuboidShape(4.0D, 0.0D, 4.0D, 12.0D, 8.0D, 12.0D);
+        //return VoxelShapes.cuboid(4.0D, 0.0D, 4.0D, 12.0D, 8.0D, 12.0D);
+        return VoxelShapes.cuboid(0.35D, 0.0D, 0.35D, 0.65D, 0.5D, 0.65D);
+    }
+}
